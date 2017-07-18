74180ce72f7be328e60f1c7cdf818eb1b7745069
Ted Wu <tedwu2016@u.northwestern.edu>
2017-07-18 00:17:29 -0700

    taxonomies dict

--- helpers/parseEvents.js
+++ helpers/parseEvents.js
@@ -7,7 +7,9 @@ exports.parseTicketMaster = (data) => {
   console.log("tm data", data);
 }
 
-exports.getTaxonomies = (taxonomies) => {
-  return taxonomies.map(taxonomy => taxonomy.slug);
-}
+exports.getTaxonomies = (taxonomies) => taxonomies.map(taxonomy => {
+  const obj = {};
+  obj[taxonomy.slug] = taxonomy.name;
+  return obj;
+});
 
--- taxonomies.js
+++ taxonomies.js
@@ -1,4 +1,204 @@
-
-
+{
+"taxonomies": [
+    {
+        "sports": "Sports"
+    },
+    {
+        "baseball": "Baseball"
+    },
+    {
+        "mlb": "MLB Baseball"
+    },
+    {
+        "ncaa_baseball": "NCAA Baseball"
+    },
+    {
+        "minor_league_baseball": "Minor League Baseball"
+    },
+    {
+        "football": "Football"
+    },
+    {
+        "nfl": "NFL Football"
+    },
+    {
+        "ncaa_football": "College Football"
+    },
+    {
+        "basketball": "Basketball"
+    },
+    {
+        "nba": "NBA Basketball"
+    },
+    {
+        "ncaa_basketball": "College Basketball"
+    },
+    {
+        "ncaa_womens_basketball": "College Women's Basketball"
+    },
+    {
+        "wnba": "WNBA Basketball"
+    },
+    {
+        "nba_dleague": "NBA D-League"
+    },
+    {
+        "hockey": "Hockey"
+    },
+    {
+        "nhl": "NHL Hockey"
+    },
+    {
+        "ncaa_hockey": "College Hockey"
+    },
+    {
+        "minor_league_hockey": "Minor League Hockey"
+    },
+    {
+        "soccer": "Soccer"
+    },
+    {
+        "mls": "MLS Soccer"
+    },
+    {
+        "ncaa_soccer": "College Soccer"
+    },
+    {
+        "european_soccer": "European Soccer"
+    },
+    {
+        "international_soccer": "International Soccer"
+    },
+    {
+        "world_cup": "2014 World Cup"
+    },
+    {
+        "us_minor_league_soccer": "US Minor League Soccer"
+    },
+    {
+        "auto_racing": "Auto Racing"
+    },
+    {
+        "nascar": "NASCAR"
+    },
+    {
+        "nascar_sprintcup": "NASCAR Sprint Cup Series"
+    },
+    {
+        "nascar_nationwide": "NASCAR Nationwide Series"
+    },
+    {
+        "indycar": "Indycar"
+    },
+    {
+        "f1": "F1 Racing"
+    },
+    {
+        "monster_truck": "Monster Truck"
+    },
+    {
+        "motocross": "Motocross"
+    },
+    {
+        "golf": "Golf"
+    },
+    {
+        "pga": "PGA Golf"
+    },
+    {
+        "lpga": "LPGA Golf"
+    },
+    {
+        "fighting": "Fighting"
+    },
+    {
+        "boxing": "Boxing"
+    },
+    {
+        "mma": "MMA"
+    },
+    {
+        "wrestling": "Wrestling"
+    },
+    {
+        "wwe": "WWE"
+    },
+    {
+        "tennis": "Tennis"
+    },
+    {
+        "animal_sports": "Animal Sports"
+    },
+    {
+        "horse_racing": "Horse Racing"
+    },
+    {
+        "rodeo": "Rodeo"
+    },
+    {
+        "extreme_sports": "Extreme Sports"
+    },
+    {
+        "olympic_sports": "Olympic Sports"
+    },
+    {
+        "": "Concerts"
+    },
+    {
+        "concert": "Concert"
+    },
+    {
+        "music_festival": "Music Festivals"
+    },
+    {
+        "theater": "Theater"
+    },
+    {
+        "classical": "Classical"
+    },
+    {
+        "classical_opera": "Classical Opera"
+    },
+    {
+        "classical_vocal": "Classical Vocal"
+    },
+    {
+        "classical_orchestral_instrumental": "Classical Orchestral"
+    },
+    {
+        "cirque_du_soleil": "Cirque Du Soleil"
+    },
+    {
+        "broadway_tickets_national": "Broadway Shows"
+    },
+    {
+        "comedy": "Comedy"
+    },
+    {
+        "family": "Family Entertainment"
+    },
+    {
+        "dance_performance_tour": "Dance Shows"
+    },
+    {
+        "film": "Film"
+    },
+    {
+        "literary": "Literary"
+    },
+    {
+        "circus": "Circus"
+    },
+    {
+        "addon": "addon"
+    },
+    {
+        "parking": "parking"
+    },
+    {
+        "club_passes": "club_passes"
+    }
+]
+}
 
 
